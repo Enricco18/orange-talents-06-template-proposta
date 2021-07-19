39cd6334740cab7493708022444b39d0dc5237db
Enricco Diniz <enricco.amaral@zup.com.br>
2021-07-19 14:52:17 -0300

    Setup inicial do projeto | 19/7 15h

---
+++ src/test/java/br/com/zupacademy/enricco/proposta/PropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.enricco.proposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
